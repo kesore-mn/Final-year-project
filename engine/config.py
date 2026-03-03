0a0869c6de97b923dd904fbfc15a4e2004d87a06
kesore-mn <kingkesore@gmail.com>
2026-03-03 23:01:25 +0530

    Modify API key placeholders in config.py
    Updated API keys to indicate user should set them.

--- engine/config.py
+++ engine/config.py
@@ -1,5 +1,5 @@
 ASSISTANT_NAME = "jarvis"
-LLM_KEY = "AIzaSyAgXi8OUc-lu2rh0vpo12vO02vLoBhoe8U"
-GROQ_API_KEY = "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" # User needs to set this
+LLM_KEY = "get api key from gemini"
+GROQ_API_KEY = "get api key from groq ai" # User needs to set this
 PREFERRED_LANGUAGE = "en" # Default to English
 OFFLINE_MODE = False
